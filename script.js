8808d0b60effbfe9875a8210001089a96bfabdf9
pasayevdemir <pashayev_damir@mail.ru>
2025-10-24 16:28:11 +0400

    have changed header and base container by Damir. have added login poopup element into website. have wrote js for popup. good luck Fatima))

--- script.js
+++ script.js
@@ -19,3 +19,56 @@ faqItems.forEach((item, index) => {
     // əgər isActive idisə, yuxarıda hamısını təmizləmişik (yəni heç biri açıq qalmır)
   });
 });
+
+// Temp database for login
+const users = [
+  { login: "admin", password: "admin" }
+];
+
+const popupLogin = document.querySelector("#popupLogin");
+let loginInput = document.querySelector("#login")?.value.trim("");
+let passwordInput = document.querySelector("#password")?.value.trim("");
+
+const btnLoginForwardUI = document.querySelector("#btnLoginForwardUI");
+//Login popup open
+btnLoginForwardUI.addEventListener("click", ForwardUI);
+function ForwardUI() {
+  popupLogin.classList.toggle("active");
+  document.body.style.overflow = "hidden";
+  clearInputs()
+}
+//Login popup close
+const btnClose = document.querySelector("#btnClose");
+btnClose.addEventListener("click", CloseLoginUI);
+function CloseLoginUI() {
+  popupLogin.classList.remove("active");
+  document.body.style.overflow = "auto";
+  clearInputs()
+}
+
+const btnLogin = document.querySelector("#btnLogin");
+btnLogin.addEventListener("click", CheckLogin);
+function CheckLogin() {
+  loginInput = document.querySelector("#login")?.value.trim("");
+  passwordInput = document.querySelector("#password")?.value.trim("");
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    if (user.login == loginInput && user.password == passwordInput) {
+      CloseLoginUI();
+      clearInputs()
+    }
+  }
+}
+
+function clearInputs(){
+  loginInput = document.querySelector("#login");
+  passwordInput = document.querySelector("#password");
+  loginInput.value = "";
+  passwordInput.value = ""; 
+}
+
+console.log([btnLogin]);
+
+
+
+// document.body.style.overflow = "hidden";  
