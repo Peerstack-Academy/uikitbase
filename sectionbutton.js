a0b1986d4165c29340442e785458a035d2fa93d6
Shukurov.Cavid <cavidwukurov917@gmail.com>
2025-10-30 23:21:07 +0400

    responsivlik yoxdu

--- sectionbutton.js
+++ sectionbutton.js
@@ -2,14 +2,15 @@ document.querySelectorAll('.section-btn1').forEach(btn => {
   btn.addEventListener('click', () => {
     const cavab = document.getElementById(btn.getAttribute('data-id'));
     const aciqdir = btn.getAttribute('data-ac') === 'true';
+    const li = btn.closest('.section-open1');
     btn.setAttribute('data-ac', !aciqdir);
+    
     cavab.hidden = aciqdir;
 
-     const li = btn.closest('.section-open1');
     if (!aciqdir) {
       li.classList.add('active');
     } else {
-      li.classList.remove('active');
+      li.classList.remove('active'); 
     }
   });
 });
